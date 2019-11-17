c2d61d157f2f5da832304eb0ebdf8b9b37045e30
Guy <guyskv@gmail.com>
2019-11-17 12:13:34 +0200

    Program skeleton - add login method

--- app/src/main/java/com/example/class3/MainActivity.java
+++ app/src/main/java/com/example/class3/MainActivity.java
@@ -4,18 +4,45 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText main_EDT_input;
+    private TextView main_LBL_info;
+    private Button main_BTN_check;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        print();
+
+        main_EDT_input = findViewById(R.id.main_EDT_input);
+        main_LBL_info = findViewById(R.id.main_LBL_info);
+        main_BTN_check = findViewById(R.id.main_BTN_check);
+
+
+        main_BTN_check.setOnClickListener(myListener);
+        Log.d("", "");
+
     }
 
-    private void print() {
-        Log.d("pttt", "Printed ");
+    View.OnClickListener myListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            start();
+        }
+    };
+
+    private void start() {
+        String input = main_EDT_input.getText().toString();
+        boolean result = MyLogic.buttonCheck(input);
+
+        main_LBL_info.setText("Result= " + result);
     }
+
 }
---
+++ app/src/main/java/com/example/class3/MyLogic.java
@@ -0,0 +1,16 @@
+package com.example.class3;
+
+public class MyLogic {
+
+    /**
+     * check code string validation
+     * @param input - the string to check
+     * @return if all braces are ok
+     */
+    public static boolean buttonCheck(String input) {
+        // TODO: 2019-11-13 generate code
+
+
+        return false;
+    }
+}
